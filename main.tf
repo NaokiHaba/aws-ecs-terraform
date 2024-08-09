312295540649237ca6f9e1e76e82a1263b5085bd
Naoki Haba <naosprintrunner6385@gmail.com>
2024-08-09 15:51:23 +0900

    chore: Update S3 bucket name for Terraform state file storage

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # 状態ファイルの保存先としてAWS S3を使用する
-    bucket         = "terraform-state-bucket-dev"
+    bucket         = "aws-ecs-terraform-tfstate-2"
     key            = "terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "terraform-state-lock-dynamo"
