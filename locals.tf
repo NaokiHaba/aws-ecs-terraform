ed39227f3fa64631aaca787542c81f9ebf75405f
Naoki Haba <naosprintrunner6385@gmail.com>
2024-08-09 20:57:19 +0900

    chore: Update tfcmt version to v4.0.0 and fix indentation in terraform.yaml

--- locals.tf
+++ locals.tf
@@ -1,5 +1,13 @@
 locals {
+  # プロジェクト名を設定
+  # 現在の作業ディレクトリ名からアンダースコアをハイフンに置換して使用
   name   = replace(basename(path.cwd), "_", "-")
+
+  # AWSリージョンを設定
+  # 東京リージョン（ap-northeast-1）を使用
   region = "ap-northeast-1"
+
+  # アプリケーション名を設定
+  # このプロジェクトで使用するGoアプリケーションの名前
   app    = "go-simple-server"
 }
--- main.tf
+++ main.tf
@@ -1,21 +1,26 @@
+# AWSプロバイダーの設定
+# ローカル変数から地域を指定
 provider "aws" {
   region = local.region
 }
 
+# Terraformの設定
 terraform {
+  # S3バックエンドの設定
+  # 状態ファイルをS3に保存し、DynamoDBでロックを管理
   backend "s3" {
-    bucket         = "aws-ecs-terraform-tfstate-2"
-    key            = "terraform.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "aws-ecs-terraform-tfstate-locking"
-    encrypt        = true
+    bucket         = "aws-ecs-terraform-tfstate-2"  # 状態ファイルを保存するS3バケット
+    key            = "terraform.tfstate"            # 状態ファイルの名前
+    region         = "ap-northeast-1"               # S3バケットのリージョン
+    dynamodb_table = "aws-ecs-terraform-tfstate-locking"  # ステートロック用のDynamoDBテーブル
+    encrypt        = true                           # 状態ファイルの暗号化を有効化
   }
 
+  # 必要なプロバイダーの指定
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+      source  = "hashicorp/aws"  # AWSプロバイダーのソース
+      version = "~> 5.0"         # AWSプロバイダーのバージョン指定（5.x系）
     }
   }
 }
-
